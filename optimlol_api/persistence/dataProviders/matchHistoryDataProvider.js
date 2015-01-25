b44c081bc8bdaf4be8d604926034900e0855f655
jmadewell <jmadewell@marinsoftware.com>
2015-01-25 14:59:39 -0600

    adding match history data provider

--- optimlol_api/persistence/dataProviders/matchHistoryDataProvider.js
+++ optimlol_api/persistence/dataProviders/matchHistoryDataProvider.js
@@ -4,26 +4,62 @@ module.exports = function() {
 	var self = this;
 	var _apiVersion = null;
 	var _riotApi = null;
+	var _mongoCache = null;
+	var _logger = null;
 
-	self.getRankedMatchHistory = function(region, summonerId) {
-		var matchHitoryPath = region + "/" + _apiVersion + "/stats/by-summoner/" + summonerId + "/ranked";
+	var MATCH_HISTORY_TYPES = {
+		"solo": "RANKED_SOLO_5x5",
+		"team": "RANKED_TEAM_5x5",
+		"tri": "RANKED_TEAM_3x3"
+	};
+
+	var _getMatchHistoryApi = function(region, summonerId, type, deferred) {
+		var rankedMatchHitoryPath = region + "/" + _apiVersion + "/matchhistory/" + summonerId + "?rankedQueues=" + MATCH_HISTORY_TYPES[type];
 		
-		var deferred = q.defer();
-		_riotApi.makeRequest(matchHitoryPath)
+		_riotApi.makeRequest(rankedMatchHitoryPath)
 			.then(function(matchHistoryResult) {
-				
-				deferred.resolve();
+				_mongoCache.set('matchHistory', { region: region, summonerId: summonerId, type: MATCH_HISTORY_TYPES[type] }, matchHistoryResult.data)
+					.then(function() {
+						deferred.resolve(matchHistoryResult);
+					})
+					.fail(function() {
+						// if setting cache fails, don't worry, move on.
+						deferred.resolve(matchHistoryResult);
+					});
 			})
 			.fail(function(error) {
 				deferred.reject(error);
 			});
+	}
+
+	self.getMatchHistory = function(region, summonerId, type) {
+		_logger.debug("Getting ranked match history data", summonerId, MATCH_HISTORY_TYPES[type]);
+		var deferred = q.defer();
+		_mongoCache.get('matchHistory', {region: region, summonerId: summonerId, type: MATCH_HISTORY_TYPES[type]})
+			.then(function(cachedMatchHistory) {
+				if (cachedMatchHistory.data !== null) {
+					_logger.debug("Using cached match history data");
+					deferred.resolve(cachedMatchHistory);
+				} else {
+					_getMatchHistoryApi(region, summonerId, type, deferred);
+				}
+			})
+			.fail(function(error) {
+				_getMatchHistoryApi(region, summonerId, type, deferred);
+			});
 
 		return deferred.promise;
-	}
+	};
 
 	self.init = function() {
 		var config = require('../../config');
 		_apiVersion = config.riot_api.versions.matchHistory;
+
+		var MongoCache = require('../../common/mongoCache');
+		_mongoCache = new MongoCache();
+
+		var Logger = require('../../common/logger');
+		_logger = new Logger();
 
 		var RiotApi = require('../../common/riotApi');
 		_riotApi = new RiotApi();
---
+++ optimlol_api/persistence/mongoModels/matchHistoryModel.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var q = require('q');
+
+var Schema = mongoose.Schema;
+
+var MatchHistorySchema = new Schema({
+	summonerId: { type: Number, required: true },
+	region: { type: String, required: true },
+	type: { type: String, required: true },
+	expiredTimeMinutes: { type: Number, default: 60 },
+	data: Schema.Types.Mixed,
+	created_at: { type: Date },
+	updated_at: { type: Date }
+});
+
+// every model should have a retrieve function that does any
+// special things necessary to get the proper data.
+// most of the time...this isn't an issue so really, we're using retrieve 
+// so we can always have a promise :)
+MatchHistorySchema.statics.retrieve = function(identifiers) {
+	var deferred = q.defer();
+	this.model('match_history').findOne(identifiers, function(error, result) {
+		if (error) deferred.reject(error);
+		else {
+			deferred.resolve(result);
+		}
+	});
+
+	return deferred.promise;
+}
+
+MatchHistorySchema.pre('save', function(next) {
+	var now = new Date();
+	this.updated_at = now;
+	if (!this.created_at) {
+		this.created_at = now;
+	}
+	next();
+});
+
+// mongoose makes collections plural if we don't specify an name in third parameter :[
+module.exports = mongoose.model('match_history', MatchHistorySchema, 'match_history');
