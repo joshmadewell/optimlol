fd1df60af3a5342cfb0777bcbc270ce16e1dcf85
Josh Madewell <josh.madewell@gmail.com>
2015-02-03 22:36:01 -0600

    moving knockout configuration to it's own directory/file

--- web/app/main.js
+++ web/app/main.js
@@ -11,7 +11,7 @@
 define('knockout', ko);
 define('jquery', function() { return jQuery });
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'configuration/knockoutConfiguration'],  function (system, app, viewLocator, knockoutConfiguration) {
     system.debug(false);
 
     app.title = 'OptimLoL';
@@ -26,26 +26,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
         //Look for partial views in a 'views' folder in the root.
         viewLocator.useConvention();
 
-        ko.bindingHandlers.tooltip = {
-            init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                var tooltipConfig = valueAccessor();
-
-                if (typeof(tooltipConfig) === 'object') {
-                    if (tooltipConfig.position) {
-                        $(element).attr("data-placement", tooltipConfig.position);
-                    }
-
-                    if (tooltipConfig.text) {
-                        $(element).attr("title", tooltipConfig.text)
-                    }
-                } else {
-                    $(element).attr("title", valueAccessor);
-                }
-
-                $(element).attr("data-container", 'body');
-                $(element).tooltip();
-            }
-        };
+        knockoutConfiguration.configure();
 
         //Show the app by setting the root view model for our application with a transition.
         app.setRoot('viewmodels/shell', 'entrance');
