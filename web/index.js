873b74e425a5847866744f729a33ab9ff4bc8994
jmadewell <jmadewell@marinsoftware.com>
2015-02-18 17:43:34 -0600

    compression

--- web/index.js
+++ web/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var compression = require('compression');
+//var h5bp = require('h5bp');
 var app = express();
 
 app.set('view engine', 'html');
 app.set('views', __dirname);
+//app.use(h5bp({root: __dirname}));
+app.use(compression());
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
   res.render('index.html');
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 9001);
