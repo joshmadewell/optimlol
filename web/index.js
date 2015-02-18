fbec35c2a09449fe1463c3c181ce7b49dfd40efa
jmadewell <jmadewell@marinsoftware.com>
2015-02-18 17:44:09 -0600

    Removing h5bp

--- web/index.js
+++ web/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var compression = require('compression');
-//var h5bp = require('h5bp');
 var app = express();
 
 app.set('view engine', 'html');
 app.set('views', __dirname);
-//app.use(h5bp({root: __dirname}));
 app.use(compression());
 app.use(express.static(__dirname));
 
